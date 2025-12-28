1885ed537a244f593bcc57cc9c39191ab6aa720e
Hima Krishna Priy <yadlahimakrishnapriya@gmail.com>
2025-12-28 19:52:33 +0530

    4th

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Fleet Management Dashboard</h1>
+      <p>Welcome to Fleet Management System</p>
+    </div>
+  );
+};
+
+export default Dashboard;
